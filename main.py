70503f2baa8ab8071127fa4822e48f6a14f07e2e
tnarkiv <vikrant2099ad@gmail.com>
2025-02-18 08:43:26 +0000

    Added fps

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from constants import *
 def main():
     pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("BLACK")
         pygame.display.flip()
-    
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
