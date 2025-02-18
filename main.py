63bfdc981e7694a9bcf29dfab57b2b6b76b8ad33
tnarkiv <vikrant2099ad@gmail.com>
2025-02-18 06:49:52 +0000

    Added constant to main file

---
+++ main.py
@@ -0,0 +1,13 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+import constants as cs
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {cs.SCREEN_WIDTH}")
+    print(f"Screen height: {cs.SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
