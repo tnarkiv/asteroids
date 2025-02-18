8f61959190fbc14dfd04c1626544a79dcc2a9185
tnarkiv <vikrant2099ad@gmail.com>
2025-02-18 08:58:55 +0000

    Added player on GUI

--- main.py
+++ main.py
@@ -3,17 +3,20 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0    
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)    
     while 1:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("BLACK")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
